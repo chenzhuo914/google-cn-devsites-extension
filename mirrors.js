035499f2bc58142f365208ea33c6c06363b6c960
Zhuo Chen <chenzhuo914@gmail.com>
2017-01-13 11:49:38 +0800

    Add a whitelist for URL paths that won't be transformed to CN mirrors

--- mirrors.js
+++ mirrors.js
@@ -1,11 +1,26 @@
+// These URL paths will be transformed to CN mirrors.
 var mirrors = {
   "//developers.google.com" : "//developers.google.cn",
-  "//firebase.google.com" : "//firebase.google.cn",
+  "//firebase.google.com"   : "//firebase.google.cn",
   "//developer.android.com" : "//developer.android.google.cn",
-  "//angular.io" : "//angular.cn"
+  "//angular.io"            : "//angular.cn",
 }
 
+// These URL paths are not available on CN mirrors, therefore won't be transformed.
+var whitelist = [
+  "//developers.google.com/groups",
+  "//developers.google.com/events",
+]
+
 function mirrorUrl(url) {
+  // Check for whitelisting.
+  for (var key in whitelist) {
+    if (url.includes(whitelist[key])) {
+      return url;
+    }
+  }
+  
+  // Check for mapping.
   for (var key in mirrors) {
   	if (url.includes(key)) {
   		url = url.replace(key, mirrors[key]);
